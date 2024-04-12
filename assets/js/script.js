da6701bca7ad7845f5b9d841f70df279af8600c4
Huang Po-Wei <71061276+georgepwhuang@users.noreply.github.com>
2024-04-13 01:08:44 +0800

    Email shielding

--- assets/js/script.js
+++ assets/js/script.js
@@ -269,3 +269,47 @@ function findAux(item) {
 Array.from(document.getElementsByClassName('last-modified')).forEach(element => {
   element.innerHTML= new Date(document.lastModified).toLocaleString('en-US', { month: 'short', year: "numeric"});
 });
+
+
+function encodeEmail(email, key) {
+  var encodedKey = key.toString(16);
+  var encodedString = make2DigitsLong(encodedKey);
+  var result = ""; 
+  for(var n=0; n < email.length; n++) {
+      var charCode = email.charCodeAt(n);
+      var encoded = charCode ^ key;
+      var value = encoded.toString(16);
+      result += make2DigitsLong(value);
+  }
+  result += encodedString;
+  return result;
+}
+
+function make2DigitsLong(value){
+  return value.length === 1 
+      ? '0' + value
+      : value;
+}
+
+function decodeEmail(encodedString) {
+  var email = ""; 
+  var keyInHex = encodedString.substr(encodedString.length - 2);
+  var key = parseInt(keyInHex, 16);
+  for (var n = 0; n < encodedString.length - 2; n += 2) {
+      var charInHex = encodedString.substr(n, 2)
+      var char = parseInt(charInHex, 16);
+      var output = char ^ key;
+      email += String.fromCharCode(output);
+  }
+  return email;
+}
+
+
+var protectedElements = document.getElementsByClassName("protected");
+for (var i = 0; i < protectedElements.length; i++) {
+  protectedElements[i].addEventListener("mouseover", function () { 
+    var encoded = this.getAttribute("eml");
+    var decoded = decodeEmail(encoded);
+    this.href = 'mailto:' + decoded;
+  }, {once : true});
+}
